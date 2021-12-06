4b10fd5ceca8cc75e0ffcdb33584a468fb410cff
reppc <84331772+reppc@users.noreply.github.com>
2021-12-06 07:45:58 -0600

    correccion historials

---
+++ database/migrations/2021_12_06_134120_create_historials.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistorials extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('historials', function (Blueprint $table) {
+            $table->id();
+            
+            $table->date('fecha_inicio');
+            $table->date('fecha_final');
+            $table->time('hora_inicio');
+            $table->time('hora_final');
+            $table->string('estado',100);
+            $table->string('descripcion',255);
+            
+            $table->foreignId('sensor_id')
+            ->nullable()
+            ->constrained('sensor')
+            ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('historials');
+    }
+}
